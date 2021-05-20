4c6ea23ad78363336889f02c6bf1c740b6456d9b
Moon Jaewon <jaewonmoo.keti@gmail.com>
2021-05-20 13:57:46 +0900

    1st commit

--- data_integration/data_integration.py
+++ data_integration/data_integration.py
@@ -1,14 +1,4 @@
-import numpy as np 
-from sklearn.preprocessing import MinMaxScaler 
-import pandas as pd 
-import math 
-import data_management.data_outlier as do
-
-class IntegratedData():
-    def __init__(self):
-        pass
-        
-    def simple_integration(self, data, resample_min):
+ def data_integration(self, data, resample_min):
         resample_freq = str(resample_min)+'min'
         data_key_list = list(data.keys())
         merged_data_list =[]
@@ -23,20 +13,3 @@ class IntegratedData():
         merged_data=merged_data.sort_index(axis=1)
        
         return merged_data
-    """
-    def advanced_integration(self, data, resample_min):
-        resample_freq = str(resample_min)+'min'
-        data_key_list = list(data.keys())
-        merged_data_list =[]
-        data_resample={}
-        for data_name in data_key_list:
-            data_resample[data_name] = data[data_name].resample(resample_freq, label='left').mean()
-            data_resample[data_name] = data_resample[data_name].fillna(method='bfill', limit=1)
-            data_resample[data_name] = data_resample[data_name].fillna(method='ffill', limit=1)
-            merged_data_list.append(data_resample[data_name])
-        
-        merged_data = pd.concat(merged_data_list, axis=1, join='inner')
-        merged_data=merged_data.sort_index(axis=1)
-       
-        return merged_data
-    """  
