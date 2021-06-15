4a3b1f2dbdff7645f99824cc900fb19260e4d1cb
Moon Jaewon <jaewonmoo.keti@gmail.com>
2021-06-15 19:36:39 +0900

    minor revision

--- data_integration/data_integration.py
+++ data_integration/data_integration.py
@@ -5,7 +5,8 @@ import numpy as np
 class DataIntegration():
     def __init__(self, data_partial, partial_data_info):    
         self.data_partial = data_partial
-        self.partial_data_info = partial_data_info
+        self.integration_info = partial_data_info.column_meta
+        self.partial_data_type = partial_data_info.integrated_data_type
 
         
     def simple_integration(self):
@@ -15,6 +16,11 @@ class DataIntegration():
             partial_data = self.data_partial[data_name].sort_index(axis=1)
             merged_data_list.append(partial_data)
         merged_data = pd.concat(merged_data_list, axis=1, join='outer', sort=True)#inner
+        duration = self.integration_info['overap_duration']
+        print(duration['start_time'])
+        print(duration['end_time'])
+        print(merged_data)
+        merged_data = merged_data_list[str(duration['start_time']):str(duration['end_time'])]
         return merged_data
     
     def restructured_data_with_new_frequency(self, data, frequency, method):
