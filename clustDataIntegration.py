3c7dec564b20584d72964246b5b30fa97198c2de
jwmoonP <jaewonmoon.keti@gmail.com>
2022-03-28 12:51:27 +0900

    integratipn update

--- clustDataIntegration.py
+++ clustDataIntegration.py
@@ -8,7 +8,7 @@ class ClustIntegration():
     def __init__(self):
         pass
 
-    def clustIntegrationFromInfluxSource(self, intDataInfo, process_param, re_frequency_min):
+    def clustIntegrationFromInfluxSource(self, intDataInfo, process_param, integration_freq_min, integrationMethod):
         ## Connect DB
         from KETIPreDataIngestion.KETI_setting import influx_setting_KETI as ins
         from KETIPreDataIngestion.data_influx import influx_Client
@@ -25,15 +25,25 @@ class ClustIntegration():
         imputed_datas = {}
         for key in multiple_dataset.keys():
             imputed_datas[key]=(multiple_dataset[key]["imputed_data"])
-        result = self.getIntegratedDataSetByMeta(imputed_datas, re_frequency_min)
+        if integrationMethod=="meta":
+            result = self.getIntegratedDataSetByMeta(imputed_datas, integration_freq_min)
+        elif integrationMethod=="ML":
+            result = self.getIntegratedDataSetByML(imputed_datas, integration_freq_min)
+        else:
+            result = self.getIntegratedDataSetByMeta(imputed_datas, integration_freq_min)
+
         return result
 
-    def getIntegratedDataSetByMeta(self, data_set, re_frequency_min):
+    def getIntegratedDataSetByML(self, data_set, integration_freq_min):
+        # 필요시 모 함수에서 받아오는 파라미터가 수정 및 확장되어야함 황지수씨 시작점
+        pass
+
+    def getIntegratedDataSetByMeta(self, data_set, integration_freq_min):
         from KETIPreDataIntegration.data_integration import partialDataInfo
         partial_data_info = partialDataInfo.PartialData(data_set)
         # Integration
         import datetime
-        re_frequency = datetime.timedelta(seconds= re_frequency_min*60)
+        re_frequency = datetime.timedelta(seconds= integration_freq_min*60)
         from KETIPreDataIntegration.data_integration import data_integration
         data_it = data_integration.DataIntegration(data_set)
         integrated_data_resample = data_it.dataIntegrationByMeta(re_frequency, partial_data_info.column_meta)
