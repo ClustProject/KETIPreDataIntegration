8157a3603a24bd817b5179038e15d3a8db777892
hwangjisoo <ghkdwltn8@gmail.com>
2022-10-04 11:07:20 +0900

    rnn ae code update

--- clustDataIntegration.py
+++ clustDataIntegration.py
@@ -231,7 +231,7 @@ class ClustIntegration():
         if transfomrParam["window_size"] == "inputData Length-1":
             transfomrParam["window_size"] = len(dintegrated_data) - 1
         if model == "RNN_AE":
-            alignment_result = RNNAEAlignment.Alignment().RNN_AE(dintegrated_data, transfomrParam)
+            alignment_result = RNNAEAlignment.RNN_AE(dintegrated_data, transfomrParam)
         else :
             print('Not Available')
             
